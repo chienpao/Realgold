0efb6462058e596a2276b5afa53dabfa3b17f79f
Pao <designer.pao@gmail.com>
2015-01-08 19:06:14 +0800

    [Realgold] Add handler and thread to get real gold.
    Single thread is create every time, does it have the other
    good way?

--- app/src/main/java/app/wordpress/me/synology/chienpao/realgold/MainActivity.java
+++ app/src/main/java/app/wordpress/me/synology/chienpao/realgold/MainActivity.java
@@ -1,13 +1,18 @@
 package app.wordpress.me.synology.chienpao.realgold;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -16,34 +21,69 @@ import java.net.URL;
 
 public class MainActivity extends ActionBarActivity {
 
+    public String temp = "";
+    private Thread mThread;
+    private Handler handler;
+    private TextView textView;
+    private Button button;
+    private boolean running = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.test);
+        button = (Button)findViewById(R.id.button);
 
         parseHTML();
+        handleText();
     }
 
-    public void parseHTML(){
+    public void parseHTML() {
 
-        new Thread(new Runnable(){
+        running = true;
+        mThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 URL url = null;
-                // TODO Auto-generated method stub
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect("http://rate.bot.com.tw/Pages/Static/UIP005.zh-TW.htm").get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Element id = doc.getElementById("GoldBankBookForTWD");
+                Elements tables = id.getElementsByClass("decimal");
 
-                    Document doc = null;
-                    try {
-                        doc = Jsoup.connect("http://rate.bot.com.tw/Pages/Static/UIP005.zh-TW.htm").get();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    Elements test = doc.getElementsByClass("goldName0");
-                    Log.i("Pao", test.text());
-
+                temp = tables.text().substring(12,16);
+                running = false;
+                Log.i("Pao", temp);
             }
-        }).start();
+        });
+        mThread.start();
+
+    }
+
+    public void handleText(){
+        handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    Log.i("Pao", mThread.getState().toString());
+                    textView.setText(temp);
+                    if(running == false)
+                        break;
+                }
+            }
+        });
+
+    }
+
+    public void setText(View view){
+        Log.i("Pao", "Button Click");
+        parseHTML();
+        handleText();
     }
 
     @Override
